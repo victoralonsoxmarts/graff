892cd93c35fbb90cf9c1989032b00cade3687846
Rodo TuBot <84582248+TuOdoo@users.noreply.github.com>
2022-12-01 10:43:00 -0600

    fix reporte

--- graff_entrada_pt/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import reports_mrp
-from . import mrp_production
-from . import res_company
--- graff_entrada_pt/models/res_company.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    iconreport = fields.Binary(
-        string="Imagen report fabricacion"
-    )
